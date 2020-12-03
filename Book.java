c2821e8b7c318c4033724216169d28ee55696320
sassassa <sasaki6503@gmail.com>
2020-12-03 21:24:27 +0900

    Book class와 Loan class의 method body를 채웠습니다.

--- Book.java
+++ Book.java
@@ -1,8 +1,8 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Book here.
  *
-  * @author (사사키 야스오, 우노 히데노리, 사토 리츠시, 2017210105 김민수)
+  * @author (2019315066 사사키 야스오, 2019315065 우노 히데노리, 2019315064 사토 리츠시, 2017210105 김민수)
  * @version (a version number or a date)
  */
 public class Book
@@ -12,19 +12,36 @@ public class Book
     private int catalogueNumber;
     private Borrower borrower;
     
-    public Book(String title, String author, int catalogueNumber)
-    {}
+    public Book(String title, String author)
+    {
+        this.title = title;
+        this.author = author;
+        this.catalogueNumber = this.hashCode();
+    }
     
     public void attachBorrower(Borrower borrower)
-    {}
+    {
+        this.borrower = borrower;
+    }
     
     public void detachBorrower()
-    {}
+    {
+        this.borrower = null;
+    }
     
     public Borrower getBorrower()
-    {}
+    {
+        return this.borrower;
+    }
     
     public boolean is_a_loan_abaliable()
-    {}
+    {
+        if(this.borrower == null){
+            return true;// able to loan
+        }
+        else{
+            return false;// unable to loan
+        }
+    }
     
 }
--- Loan.java
+++ Loan.java
@@ -2,17 +2,23 @@
 /**
  * Write a description of class Loan here.
  *
- * @author (사사키 야스오, 우노 히데노리, 사토 리츠시, 2017210105 김민수)
+ * @author (2019315066 사사키 야스오, 2019315065 우노 히데노리, 2019315064 사토 리츠시, 2017210105 김민수)
  * @version (a version number or a date)
  */
 public class Loan
 {
     private Book book;
     private Borrower borrower;
-    
+
     public Loan(Book book, Borrower borrower)
-    {}
-    
+    {
+        this.book= book;
+        this.borrower = borrower;
+    }
+
     public void detach()
-    {}
+    {
+        this.book.detachBorrower();
+        this.borrower.detachBook();
+    }
 }
